ec02b4461fa4e1c8b8acd564a5b961c28b8a60d8
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-16 13:28:01 -0700

    added hyper-demo

---
+++ 16_DEC_2014/hyper-demo/src/main.rs
@@ -0,0 +1,18 @@
+
+extern crate hyper;
+
+use std::io::net::ip::Ipv4Addr;
+use hyper::server::{Request, Response};
+
+static PHRASE: &'static [u8] = b"Hello World!";
+
+fn hello(_: Request, res: Response) {
+    let mut res = res.start().unwrap();
+    res.write(PHRASE).unwrap();
+    res.end().unwrap();
+}
+
+fn main() {
+    hyper::Server::http(Ipv4Addr(127, 0, 0, 1), 3000).listen(hello).unwrap();
+    println!("Listening on http://127.0.0.1:3000");
+}
--- 16_DEC_2014/notes.md
+++ 16_DEC_2014/notes.md
@@ -5,4 +5,9 @@ notes
 ### What's New:
 
 + [Train to 1.0](http://blog.rust-lang.org/2014/12/12/1.0-Timeline.html)
++ [Blazing (seriously faster than light) Fast HTTP Server](https://github.com/hyperium/hyper)
 + [Templating for Content Generation](http://rustache.github.io/)
++ [Rusti REPL](https://github.com/murarth/rusti)
++ [Terminal Based Text Editor](https://github.com/gchp/iota)
+
+
