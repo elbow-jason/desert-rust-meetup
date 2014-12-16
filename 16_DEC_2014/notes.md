714cdc022e453265c89791311fdab3e43d7cb4a7
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-16 11:59:10 -0700

    added ownership and notes.md

---
+++ 16_DEC_2014/notes.md
@@ -0,0 +1,8 @@
+notes
+=====
+
+
+### What's New:
+
++ [Train to 1.0](http://blog.rust-lang.org/2014/12/12/1.0-Timeline.html)
++ [Templating for Content Generation](http://rustache.github.io/)
--- 16_DEC_2014/ownership/src/main.rs
+++ 16_DEC_2014/ownership/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+    {
+        let x = box 5i;
+    }
 }
+
+
+
+/*
+{
+    int *x = malloc(sizeof(int));
+
+    // we can now do stuff with our handle x
+    *x = 5;
+
+    free(x);
+}
+*/
