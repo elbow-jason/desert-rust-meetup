9eadea355b92a54ab6a9716d376b2771354252d0
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-17 16:21:12 -0700

    iterate a for loop with a map on it

--- 16_DEC_2014/inf_iter/src/main.rs
+++ 16_DEC_2014/inf_iter/src/main.rs
@@ -1,8 +1,8 @@
 
 fn main() {
-  for mut y in std::iter::count(1i, 5i).map(|x| x+1) {
-    y = y + 1;
-    println!("y is {}", y);
+  let mut counted = 0i;
+  for mut y in std::iter::count(1i, 1i).map(|x| x+1 ) {
+    println!("count is {}", y);
   }
 }
 
